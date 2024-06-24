513361da38eabed4ba84b9a4cdb006430419ba98
unknown <oyelekemmanuel@gmail.com>
2024-06-24 21:02:46 +0100

    madae changes

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGING,
-  appId: process.env.APPID,
+  apiKey: "AIzaSyDgQCPKlcucWihL6-8pdyp4M377TMNQulc",
+  authDomain: "outside-72d66.firebaseapp.com",
+  projectId: "outside-72d66",
+  storageBucket: "outside-72d66.appspot.com",
+  messagingSenderId: "4601109032",
+  appId: "1:4601109032:web:157d9526d4abb3e681f31d",
 };
 
 const app = initializeApp(firebaseConfig);
